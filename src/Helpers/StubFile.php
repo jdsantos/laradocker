4c88ee54dd6bda83de3856f08ec2ea7576eec468
Jorge Santos <jdsantos@users.noreply.github.com>
2024-09-02 14:06:30 +0100

    Update StubFile.php

--- src/Helpers/StubFile.php
+++ src/Helpers/StubFile.php
@@ -2,4 +2,23 @@
 
 namespace Jdsantos\Laradocker\Helpers;
 
-class StubFile {}
+class StubFile
+{
+
+public static function replaceLineWithWordInFile($filePath, $searchWord, $replacementString)
+    {
+        // Read the entire file into an array of lines
+        $fileContents = file($filePath);
+
+        // Iterate through each line
+        foreach ($fileContents as &$line) {
+            // Check if the line contains the search word
+            if (strpos($line, $searchWord) !== false) {
+                // Replace the entire line with the replacement string
+                $line = $replacementString.PHP_EOL;
+            }
+        }
+
+        return implode('', $fileContents);
+    }
+}
